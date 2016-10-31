c0b18f57f0dc3bcd310ccdadc8cedcfa3176b3b2
Joshua Ryder <kiksen@Mortens-MacBook-Pro-3.local>
2016-10-31 09:03:04 +0100

    Fixed version.json on releases

--- after.sh
+++ after.sh
@@ -1,6 +1,9 @@
 
-echo "Pushing to docker with tag $TRAVIS_TAG ..."
+echo "Pushing to docker"
 docker push kiksen1987/nodetddci:latest
 
-docker tag kiksen1987/nodetddci kiksen1987/nodetddci:$TRAVIS_TAG
-docker push kiksen1987/nodetddci:$TRAVIS_TAG
+if [ ! -z $TRAVIS_TAG ]; then
+    echo "Pushing to docker with tag $TRAVIS_TAG ..."
+    docker tag kiksen1987/nodetddci kiksen1987/nodetddci:$TRAVIS_TAG
+    docker push kiksen1987/nodetddci:$TRAVIS_TAG
+fi
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 touch version.json
-echo "{ \"version\" : \"$TRAVIS_JOB_NUMBER\" }" > version.json
+echo "{ \"version\" : \"$TRAVIS_TAG\" }" > version.json
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t kiksen1987/nodetddci .
