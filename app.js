266b293c944f9cc39b7a30a38d4cbd0d423b5118
kiksen1987 <joshua.ryder@outlook.com>
2016-11-03 21:38:39 +0100

    Trying to find public ip within the container

--- app.js
+++ app.js
@@ -8,27 +8,27 @@ var ifaces = os.networkInterfaces();
 const app = express();
 
 app.get('/', (req, res) => {
-  let result = `Hello world from TopSwagCode! We are currently running version: ${version.version}` + os.EOL;
+  let result = `Hello world from TopSwagCode! We are currently running version: ${version.version}` + "<br/>";
 
-  Object.keys(ifaces).forEach(function (ifname) {
+  
+Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0;
 
   ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
-        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-        return;
-      }
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      result += ifname + ':' + alias +"," +iface.address + " COUNT: " + alias  +"<br/>";
+    }
 
-      if (alias >= 1) {
-        // this single interface has multiple ipv4 addresses
-        result += "IP "  + ifname + ':' + alias, iface.address + os.EOL;
-      } else {
-        // this interface has only one ipv4 adress
-        result += "IP "  + ifname, iface.address + os.EOL;
-      }
-      ++alias;
-    });
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      result +=ifname + ':' + alias +"," + iface.address+ " COUNT: " + alias + "<br/>";
+    } else {
+      // this interface has only one ipv4 adress
+      result += ifname + "," + iface.address+ " COUNT: " + alias + "<br/>";
+    }
+    ++alias;
   });
+});
 
 
   res.send(result);
